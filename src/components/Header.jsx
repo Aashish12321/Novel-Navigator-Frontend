4cc6e9ba1f163efdbb984a2fe0028e5fb477fc66
Ashwini <076bct012.ashwini@pcampus.edu.np>
2023-01-30 23:26:41 +0545

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav>
+      <a href="#" className="nav--title">
+        Novel Navigator
+      </a>
+      <a href="#" className="nav--sub">
+        Top 50 Books
+      </a>
+      <a href="#" className="nav--sub">
+        Get recommendations
+      </a>
+      <a href="#" className="nav--sub">
+        About
+      </a>
+    </nav>
+  );
+};
+
+export default Header;
