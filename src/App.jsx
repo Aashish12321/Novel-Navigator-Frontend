d8258afca9586911bfedba628eece2a45283ae65
Ashwini <076bct012.ashwini@pcampus.edu.np>
2023-02-01 22:40:57 +0545

    switched to multipage using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react";
-import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import Sample from "./Pages/Sample";
 
 function App() {
-  return <Header />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/about" element={<Sample />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
